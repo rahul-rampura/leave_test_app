6b180a91b789152712555f96cdd358c67e079bc3
Rahul_Rampura <rahul.rampura@gmail.com>
2016-09-14 16:27:45 +0530

    Login Page using Google OAuth2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
 get   '/login', :to => 'sessions#new', :as => :login
-match '/auth/:provider/callback', :to => 'sessions#create'
-match '/auth/failure', :to => 'sessions#failure'
+match '/auth/:provider/callback', :to => 'sessions#create', via: "get"
+match '/auth/failure', :to => 'sessions#failure', via: "delete"
+match '/logout', :to => 'sessions#destroy', via: "delete"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
